67b3842b8b832ccf7fab6ae486062ed7ea3a4e75
Thiago Pereira <thiagorpereira7@gmail.com>
2020-01-28 00:25:04 -0200

    Return dev's after filter to frontend using websocket

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const api = require('../services/api');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
 
@@ -38,6 +39,17 @@ module.exports = {
         techs: techsArray,
         location,
       })
+
+      /**
+       * Destinatarios que receberão o new-dev /
+       * Filtra as conexões que estão há no máx 10 km de distancia
+       * e que o novo dev tenha pelo menos uma das tecnologias
+       */
+      const sendSocketMessageTo = findConnections(
+        { latitude, longitude },
+        techsArray, 
+      )
+      sendMessage(sendSocketMessageTo, 'new-dev', dev) ;
     }
     
     return res.json(dev);
